5cdc38920dee3a29722f08574db9e16d030a152a
inoveAlumnos <alumnos@inove.com.ar>
2023-08-10 13:40:11 -0300

    Fix typo

--- ejemplo_3_4/src/routes/RutaProtectedRedirect.jsx
+++ ejemplo_3_4/src/routes/RutaProtectedRedirect.jsx
@@ -2,22 +2,18 @@ import { useContext } from 'react'
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-const RutaProtected = () => {
+const RutaProtectedRedirect = () => {
     const { auth } = useContext(AuthContext);
     const location = useLocation();
 
     if (!auth) {
-        // usuario no logeado, se lo redirige a login
-        // return <Navigate to="/login" replace />
-
         // Se agrega en la URL el destino deseado
         // luego del login --> ?next=<url>
         // en los query parameters de la URL
         return <Navigate to={`/login?next=${location.pathname}`} replace />
-
     }
 
     // Usuario logeado, se renderiza la vista
     return <Outlet />;
 };
-export default RutaProtected;
+export default RutaProtectedRedirect;
