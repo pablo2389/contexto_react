22b19f1e7ae7ce55f5369d47b3061bf19d48f138
inoveAlumnos <alumnos@inove.com.ar>
2023-08-09 23:59:10 -0300

    Fix typo

--- ejemplo_2/src/context/AuthContext.js
+++ ejemplo_2/src/context/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthContext = createContext(defaultValue);
 export function initAutenticacion() {
     const isAuthenticated = sessionStorage.getItem('isAuthenticated');
     console.log(`autenticación almacenada: ${isAuthenticated}`)
-    return Boolean(isAuthenticated);  // FOrzar que el dato sea interpretado como true/false (boolean)
+    return Boolean(isAuthenticated);  // Forzar que el dato sea interpretado como true/false (boolean)
   }
   
   export function initUser() {
